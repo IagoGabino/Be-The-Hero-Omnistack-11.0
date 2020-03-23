2c3e7e2a4926c9a4967476f04b1913c48c577f1f
Iago Gabino <iagogabinogoncalves@gmail.com>
2020-03-23 18:16:08 -0300

    adicionando pasta backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({
+    evento: "Semana Omnistack",
+    aluno: "Iago Gabino"
+  });
+});
+
+app.listen(3333);
